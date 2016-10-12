ed930406b1804744f8cbb0084da22a849e9359eb
Yose <youness;chetoui@gmail.com>
2016-10-12 09:53:32 +0200

    adda image for the article

--- src/DAO/ArticleDAO.php
+++ src/DAO/ArticleDAO.php
@@ -35,6 +35,7 @@ class ArticleDAO extends DAO
         $article->setId($row['art_id']);
         $article->setTitle($row['art_title']);
         $article->setContent($row['art_content']);
+		$article->setImage($row['art_image']);
         return $article;
     }
 	
